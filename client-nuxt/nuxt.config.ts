037eca612ce6d56b8fd85f8ac0d95c3454ff7f9d
nickxrogers <62162474+nickxrogers@users.noreply.github.com>
2025-12-23 14:22:45 -0600

    Fix render.yaml property (url->host) and update Nuxt config for API_HOST

--- client-nuxt/nuxt.config.ts
+++ client-nuxt/nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:5001/YOUR_PROJECT_ID/us-central1/api',
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || (process.env.NUXT_PUBLIC_API_HOST ? `https://${process.env.NUXT_PUBLIC_API_HOST}` : 'http://localhost:5001/YOUR_PROJECT_ID/us-central1/api'),
       firebaseApiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY || '',
       firebaseAuthDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
       firebaseProjectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID || '',
@@ -34,12 +34,12 @@ export default defineNuxtConfig({
 
   compatibilityDate: '2024-07-11',
 
-  eslint: {
-    config: {
-      stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
-  }
+  //   eslint: {
+  //     config: {
+  //       stylistic: {
+  //         commaDangle: 'never',
+  //         braceStyle: '1tbs'
+  //       }
+  //     }
+  //   }
 })
